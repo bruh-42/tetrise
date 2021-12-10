8d0b2789d922fe8b55094436cb7691895b3372cb
bruh-42 <95770866+bruh-42@users.noreply.github.com>
2021-12-10 15:11:08 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let sprite: game.LedSprite = null
+input.onButtonPressed(Button.A, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    sprite.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    sprite.change(LedSpriteProperty.X, 1)
+})
+basic.forever(function () {
+    basic.pause(1000)
+    sprite = game.createSprite(2, 0)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(1000)
+    sprite = game.createSprite(2, 0)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(1000)
+})
+basic.forever(function () {
+	
+})
