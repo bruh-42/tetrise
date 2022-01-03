22280bfcad1cc564d65fb02dfc6803f228d8c188
bruh-42 <95770866+bruh-42@users.noreply.github.com>
2022-01-03 11:31:19 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,77 @@
 let sprite: game.LedSprite = null
+input.onPinPressed(TouchPin.P0, function () {
+    sprite = game.createSprite(2, 0)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(2000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+    basic.pause(5000)
+    sprite.change(LedSpriteProperty.Y, 1)
+})
 input.onButtonPressed(Button.A, function () {
     sprite.change(LedSpriteProperty.X, -1)
 })
 input.onPinPressed(TouchPin.P2, function () {
     sprite.change(LedSpriteProperty.Y, 1)
 })
+input.onButtonPressed(Button.AB, function () {
+    game.resume()
+})
 input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
 })
-basic.forever(function () {
-    basic.pause(1000)
-    sprite = game.createSprite(2, 0)
-    basic.pause(2000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(2000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(5000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(5000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(1000)
-    sprite = game.createSprite(2, 0)
-    basic.pause(2000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(2000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(5000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(5000)
-    sprite.change(LedSpriteProperty.Y, 1)
-    basic.pause(1000)
+input.onPinPressed(TouchPin.P1, function () {
+    game.pause()
 })
 basic.forever(function () {
-    basic.pause(2000)
-    if (sprite.get(LedSpriteProperty.Y) == 4) {
-        if (sprite.get(LedSpriteProperty.Y) == 4) {
-            sprite.change(LedSpriteProperty.Y, -1)
-            if (sprite.get(LedSpriteProperty.Y) == 3) {
-                sprite.change(LedSpriteProperty.Y, -1)
-                if (sprite.get(LedSpriteProperty.Y) == 2) {
-                    sprite.change(LedSpriteProperty.Y, -1)
-                }
+    if (input.pinIsPressed(TouchPin.P0) == true) {
+        if (sprite.get(LedSpriteProperty.Y) == 0) {
+            if (sprite.get(LedSpriteProperty.X) == 1) {
+                sprite.delete()
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0) == true) {
+        if (sprite.get(LedSpriteProperty.Y) == 0) {
+            if (sprite.get(LedSpriteProperty.X) == 2) {
+                sprite.delete()
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0) == true) {
+        if (sprite.get(LedSpriteProperty.Y) == 0) {
+            if (sprite.get(LedSpriteProperty.X) == 0) {
+                sprite.delete()
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (sprite.isDeleted() == true) {
+        basic.showString("game over")
+        music.playMelody("C5 A F D C C C C ", 120)
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0) == true) {
+        if (sprite.get(LedSpriteProperty.Y) == 0) {
+            if (sprite.get(LedSpriteProperty.X) == 3) {
+                sprite.delete()
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0) == true) {
+        if (sprite.get(LedSpriteProperty.Y) == 0) {
+            if (sprite.get(LedSpriteProperty.X) == 4) {
+                sprite.delete()
             }
         }
     }
